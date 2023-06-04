643b322cc6a72fc9fe1fd3c41b8d4efe3b95b82c
Cameron Chandler <webstormcam@gmail.com>
2023-06-04 11:58:16 -0400

    Starting the first card on mobile
    Putting in all the basic text, then going to put in the background and style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   content: ["./src/**/*.{html,js}",
             "./*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+    'page-back':'#ecf2ff',
+    'white':'#ffffff',
+    'red':'#ff5757',
+    'orangey':'#ffb01f',
+    'green':'	#00bd91',
+    'cobalt':'#1125d4',
+    'backgrad1':'hsla(252, 100%, 67%, 1)',
+    'backgrad2':'hsla(252, 100%, 63%, 1)',
+    'backgrad3':'hsla(241, 81%, 54%, 1)',
+    'totalgrad':' linear-gradient(180deg, #7755FF 0%, #6943FF 0.01%, #2F2CE9 100%)'
+
+    },
+    extend: {
+      fontFamily:{
+        HankenGrotesk:['Hanken Grotesk','sans-serif']
+      },
+    },
   },
   plugins: [],
 }
